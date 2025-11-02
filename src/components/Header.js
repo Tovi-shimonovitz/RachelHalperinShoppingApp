4d528d51cf336485c732cabcca9e85c7f7022472
Tovi-shimonovitz <t0534168961@gmail.com>
2025-11-02 15:55:33 +0200

    change color of title

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,23 @@ import { Link, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectItems } from '../features/cart/cartSlice';
 
-function Header() {
+function Header(props) {
   const items = useSelector(selectItems);
   const location = useLocation();
   const name="dvoraHalevi";
   const tamarrotan= "hello tamar!"
 const tamar2="good luck!!!!!"
+const myColor=props.color
 
+const colorStyle={
+  color:myColor
+}
   return (
     <header className="py-4 bg-blue-600 text-white">
       <div className="container mx-auto flex justify-between items-center px-4">
        
-        <h1 className="text-2xl font-bold">Shopping App</h1>
-        <h2 className="text-2xl font-bold">Hello to our Shopping</h2>
+        <h1 className="text-2xl font-bold" sytle={colorStyle}>Shopping App</h1>
+        <h2 className="text-2xl font-bold" sytle={colorStyle}>Hello to our Shopping</h2>
         <h1 className="text-2xl font-bold">Naama & Tovi</h1>
         <h1 className="text-2xl font-bold">Shopping App</h1>
         <h1 className >hello </h1>
